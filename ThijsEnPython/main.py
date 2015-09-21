b03897d3138e722d1c7c3e93653ea0fc44ea0872
syhtthys <naaktgeboren+thijs@gmail.com>
2015-09-21 22:24:23 +0200

    Created by Coding Ground Client

---
+++ ThijsEnPython/main.py
@@ -0,0 +1,9 @@
+print''
+print 'Thijs Python programmaatjes Menu:'
+print '1 = Risk advies'
+print '2 = Ghost game'
+menukeuze = input('Wat wil je zien? ')
+if menukeuze == 1:
+    import risk.py
+if menukeuze == 2:
+    import ghostgame.py
